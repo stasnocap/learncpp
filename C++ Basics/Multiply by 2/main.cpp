024af2e0eddd8c0521f055824f0ecd396559a9cc
Стас <stasnocap@gmail.com>
2023-11-27 17:54:31 +0300

    Add Multiply by 2

---
+++ C++ Basics/Multiply by 2/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main() {
+    std::cout << "Enter and integer: ";
+
+    int num{};
+    std::cin >> num;
+
+    std::cout << "Double " << num << " is: " << num * 2 << '\n';
+    std::cout << "Triple " << num << " is: " << num * 3 << '\n';
+    
+    return 0;
+}
